25766e0742e723c6a74a1a3bdb3dceb6cd37eb69
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2018-01-24 20:15:58 -0500

    added some components and did setting of routing

---
+++ stackoverfull/src/pages/homePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class HomePage extends Component {
+
+    render() {
+
+        return (
+            <div>
+                
+            </div>
+        );
+
+    }
+}
