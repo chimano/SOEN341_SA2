362943bcc5fb6414966ac01e483e5b2d259e4935
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2018-01-24 21:14:50 -0500

    added nav bar

---
+++ stackoverfull/src/components/navigationBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class NavigationBar extends Component {
+    render() {
+        return(
+            <div>
+                title
+            </div>
+        );
+    }
+}
