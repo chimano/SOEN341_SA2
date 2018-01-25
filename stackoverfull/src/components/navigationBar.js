c43d9e5b42f1ccaeb118232578f37ae1b98ca6bc
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2018-01-24 23:32:11 -0500

    added navbar

--- stackoverfull/src/components/navigationBar.js
+++ stackoverfull/src/components/navigationBar.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export class NavigationBar extends Component {
     render() {
-        return(
-            <div>
-                title
+        return (
+            <div className="navbar">
+                <div className="navbar__title">Stack Overfull</div>
+                <div className="navbar__search">
+                    <input
+                        className="navbar__search"
+                        type="text"
+                        placeholder="Search..."
+                        ref="filterTextInput"/>
+                </div>
+                <button className = "navbar__button">Log In</button>
+                <button className = "navbar__button">Sign Up</button>
             </div>
         );
     }
