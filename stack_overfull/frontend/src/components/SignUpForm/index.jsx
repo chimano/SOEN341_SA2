06b7ed6d78b2606980dfbb6fabe867d9a61f3c9d
anthonyle17 <anthonyle96@hotmail.com>
2018-02-08 13:00:51 -0500

    Questions Page - Issue #40 #42
    -Updated Signup form

--- stack_overfull/frontend/src/components/SignUpForm/index.jsx
+++ stack_overfull/frontend/src/components/SignUpForm/index.jsx
@@ -1,73 +1,184 @@
-import React from 'react';
-import {Form, Icon, Input, Button, Checkbox} from 'antd';
-import './index.css';
+import React from "react";
+import "./index.css";
+import { Form, Input, Tooltip, Icon, Checkbox, Button } from "antd";
+import axios from "axios";
 
 const FormItem = Form.Item;
 
 class SignUpForm extends React.Component {
-
-  handleSubmit = (e) => {
+  state = {
+    confirmDirty: false,
+    autoCompleteResult: []
+  };
+  handleSubmit = e => {
     e.preventDefault();
-    this
-      .props
-      .form
-      .validateFields((err, values) => {
-        if (!err) {
-          console.log('Received values of form: ', values);
-        }
-      });
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+        this.doUserRegisterRequest(values.username, values.password);
+      }
+    });
+  };
+  handleConfirmBlur = e => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
+  checkPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue("password")) {
+      callback("Two passwords that you enter is inconsistent!");
+    } else {
+      callback();
+    }
+  };
+  checkConfirm = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(["confirm"], { force: true });
+    }
+    callback();
+  };
+
+  doUserRegisterRequest(username, password) {
+    axios
+      .post("api/user/register/", {
+        username: username,
+        password: password
+      })
+      .then(response => this.onUserRegisterResponse(response))
+      .catch(error => this.onUserRegisterResponse(error.response));
+  }
+
+  onUserRegisterResponse(response) {
+    const { handle_close_button } = this.props;
+    console.log(
+      "Received response from the server",
+      response.request.responseURL,
+      response
+    );
+    console.log("Received user info", response.data);
+    if (!response.data.error) {
+      handle_close_button();
+    }
   }
 
   render() {
+    const { handle_close_button } = this.props;
+    const { getFieldDecorator } = this.props.form;
 
-    const {getFieldDecorator} = this.props.form;
-    const handle_close_button = this.props.handle_close_button;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 }
+      }
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0
+        },
+        sm: {
+          span: 16,
+          offset: 8
+        }
+      }
+    };
 
     return (
-
       <Form onSubmit={this.handleSubmit} className="login-form">
-        <div className="close-button" onClick={() => handle_close_button()}>&#10005;</div>
-        <span>Register</span>
-        <FormItem>
-          {getFieldDecorator('userName', {
+        <div className="close-button" onClick={() => handle_close_button()}>
+          &#10005;
+        </div>
+
+        <h3>Register</h3>
+
+        {/* Username */}
+        <FormItem
+          {...formItemLayout}
+          label={
+            <span>
+              Username&nbsp;
+              <Tooltip title="What do you want others to call you?">
+                <Icon type="question-circle-o" />
+              </Tooltip>
+            </span>
+          }
+        >
+          {getFieldDecorator("username", {
             rules: [
               {
                 required: true,
-                message: 'Please input your username!'
+                message: "Please input your nickname!",
+                whitespace: true
               }
             ]
-          })(
-            <Input
-              prefix={< Icon type = "user" style = {{ color: 'rgba(0,0,0,.25)' }}/>}
-              placeholder="Username"
-              required/>
-          )}
+          })(<Input />)}
         </FormItem>
-        <FormItem>
-          {getFieldDecorator('password', {
+
+        {/* Email */}
+        <FormItem {...formItemLayout} label="E-mail">
+          {getFieldDecorator("email", {
+            rules: [
+              {
+                type: "email",
+                message: "The input is not valid E-mail!"
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!"
+              }
+            ]
+          })(<Input />)}
+        </FormItem>
+
+        {/* Password */}
+        <FormItem {...formItemLayout} label="Password">
+          {getFieldDecorator("password", {
             rules: [
               {
                 required: true,
-                message: 'Please input your Password!'
+                message: "Please input your password!"
+              },
+              {
+                validator: this.checkConfirm
               }
             ]
+          })(<Input type="password" />)}
+        </FormItem>
+
+        {/* Confirm password */}
+        <FormItem {...formItemLayout} label="Confirm Password">
+          {getFieldDecorator("confirm", {
+            rules: [
+              {
+                required: true,
+                message: "Please confirm your password!"
+              },
+              {
+                validator: this.checkPassword
+              }
+            ]
+          })(<Input type="password" onBlur={this.handleConfirmBlur} />)}
+        </FormItem>
+
+        <FormItem {...tailFormItemLayout}>
+          {getFieldDecorator("agreement", {
+            valuePropName: "checked"
           })(
-            <Input
-              prefix={< Icon type = "lock" style = {{ color: 'rgba(0,0,0,.25)' }}/>}
-              type="password"
-              placeholder="Password"/>
+            <Checkbox>
+              I have read the <a href="">agreement</a>
+            </Checkbox>
           )}
         </FormItem>
-        <FormItem>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true
-          })(
-            <Checkbox>Remember me</Checkbox>
-          )}
-          <a className="login-form-forgot" href="">Forgot password?</a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Sign Up
+
+        <FormItem {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Register
           </Button>
         </FormItem>
       </Form>
