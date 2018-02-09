88f84d6ac9951992f9c5682986b111cc721492a1
vincentsun870530 <vincent.sun870530@gmail.com>
2018-02-09 10:48:42 -0500

    fix conflict

--- stack_overfull/frontend/src/components/NavigationBar/index.jsx
+++ stack_overfull/frontend/src/components/NavigationBar/index.jsx
@@ -1,62 +1,57 @@
 import React from "react";
-import { SearchBar } from "../SearchBar";
+import {SearchBar} from "../SearchBar";
 import "./index.css";
 
 export class NavigationBar extends React.Component {
   render() {
 
-    const { handle_signup_button, handle_signin_button, handle_career_button, handle_questions_button, logged_in, username } = this.props;
-
+    const {
+      handle_signup_button,
+      handle_signin_button,
+      handle_career_button,
+      handle_questions_button,
+      logged_in,
+      handle_logout,
+      username
+    } = this.props;
 
     return (
       <div className="navbar-wrapper">
         <div className="navbar">
           <div className="navbar__title">Stack Overfull</div>
           <div className="navbar__search">
-            <SearchBar />
+            <SearchBar/>
           </div>
-          {logged_in ? (
-            <div className="navbar__logged-in">
-              <div className="navbar__welcome">Welcome {username} !</div>
-              <button
-                className="navbar__button"
-                onClick={() => handle_logout()}
-              >
-                Log out
-              </button>
-            </div>
-          ) : (
-            <div className="navbar__auth">
-              <button
-                className="navbar__button"
-                onClick={() => handle_signin_button()}
-              >
-                Sign In
-              </button>
-              <button
-                className="navbar__button"
-                onClick={() => handle_signup_button()}
-              >
-                Sign Up
-              </button>
-            </div>
-          )}
+          {logged_in
+            ? (
+              <div className="navbar__logged-in">
+                <div className="navbar__welcome">Welcome {username}
+                  !</div>
+                <button className="navbar__button" onClick={() => handle_logout()}>
+                  Log out
+                </button>
+              </div>
+            )
+            : (
+              <div className="navbar__auth">
+                <button className="navbar__button" onClick={() => handle_signin_button()}>
+                  Sign In
+                </button>
+                <button className="navbar__button" onClick={() => handle_signup_button()}>
+                  Sign Up
+                </button>
+              </div>
+            )}
         </div>
         <div className="navbar">
           <div className="navbar__auth">
-              <button
-                className="navbar__button"
-                // onClick={() => handle_career_button()}
-              >
-                Careers
-              </button>
-              <button
-                className="navbar__button"
-                // onClick={() => handle_questions_button()}
-              >
-                Questions
-              </button>
-            </div>
+            <button className="navbar__button" onClick={() => handle_career_button()}>
+              Careers
+            </button>
+            <button className="navbar__button" onClick={() => handle_questions_button()}>
+              Questions
+            </button>
+          </div>
         </div>
       </div>
     );
--- stack_overfull/frontend/src/pages/HomePage/index.jsx
+++ stack_overfull/frontend/src/pages/HomePage/index.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  NavigationBar,
-  SignUpFormWindow,
-  SignInFormWindow,
-  QuestionList,
-  Footer
-} from "../../components";
+import {NavigationBar, SignUpFormWindow, SignInFormWindow, QuestionList, Footer} from "../../components";
 import "./index.css";
 import axios from "axios";
 
@@ -16,13 +10,18 @@ export class HomePage extends React.Component {
     this.state = {
       open_signin: false,
       open_signup: false,
-<<<<<<< HEAD
       questionList: []
     };
 
-    this.handle_signup_button = this.handle_signup_button.bind(this);
-    this.handle_signin_button = this.handle_signin_button.bind(this);
-    this.handle_close_button = this.handle_close_button.bind(this);
+    this.handle_signup_button = this
+      .handle_signup_button
+      .bind(this);
+    this.handle_signin_button = this
+      .handle_signin_button
+      .bind(this);
+    this.handle_close_button = this
+      .handle_close_button
+      .bind(this);
 
     this.getQuestionList();
   }
@@ -31,96 +30,39 @@ export class HomePage extends React.Component {
     axios
       .get("/api/question/order=asc/limit=10/")
       .then(response => {
-        this.setState({
-          questionList: response.data.question_list
-        });
+        this.setState({questionList: response.data.question_list});
         console.log(response);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   }
 
   handle_signup_button() {
-    this.setState({ open_signup: true, open_signin: false });
+    this.setState({open_signup: true, open_signin: false});
   }
 
   handle_signin_button() {
-    this.setState({ open_signin: true, open_signup: false });
+    this.setState({open_signin: true, open_signup: false});
   }
 
   handle_close_button() {
-    this.setState({ open_signin: false, open_signup: false });
+    this.setState({open_signin: false, open_signup: false});
   }
 
   render() {
     console.log(this.state);
-=======
-      logged_in: false,
-      username: ""
-    };
-
-    this.getQuestionList();
-  }
-
-  getQuestionList() {
-    axios
-      .get("/api/question/order=asc/limit=10/")
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }
-
-  handle_signup_button = () => {
-    this.setState({ open_signup: true, open_signin: false });
-  };
-
-  handle_signin_button = () => {
-    this.setState({ open_signin: true, open_signup: false });
-  };
-
-  handle_close_button = () => {
-    this.setState({ open_signin: false, open_signup: false });
-  };
-  handle_login = username => {
-    this.setState({ logged_in: true, username: username });
-  };
-  handle_logout = () => {
-    this.setState({ logged_in: false, username: "" });
-  };
-
-  render() {
-    console.log(this.state);
-
->>>>>>> 46e01a29e20e643ff7012936b85da5ecc4fccad7
     var login_box;
     if (this.state.open_signin === true) {
       login_box = (
         <div className="login-wrap">
-<<<<<<< HEAD
-          <SignInFormWindow handle_close_button={this.handle_close_button} />
-=======
-          <SignInFormWindow
-            handle_close_button={this.handle_close_button}
-            handle_login={this.handle_login}
-          />
->>>>>>> 46e01a29e20e643ff7012936b85da5ecc4fccad7
+          <SignInFormWindow handle_close_button={this.handle_close_button}/>
         </div>
       );
     } else if (this.state.open_signup === true) {
       login_box = (
         <div className="login-wrap">
-<<<<<<< HEAD
-          <SignUpFormWindow handle_close_button={this.handle_close_button} />
-=======
-          <SignUpFormWindow 
-            handle_close_button={this.handle_close_button}
-            handle_login={this.handle_login}            
-          />
->>>>>>> 46e01a29e20e643ff7012936b85da5ecc4fccad7
+          <SignUpFormWindow handle_close_button={this.handle_close_button}/>
         </div>
       );
     } else {
@@ -131,24 +73,12 @@ export class HomePage extends React.Component {
       <div>
         <NavigationBar
           handle_signup_button={this.handle_signup_button}
-          handle_signin_button={this.handle_signin_button}
-<<<<<<< HEAD
-        />{" "}
-        {login_box}
+          handle_signin_button={this.handle_signin_button}/>{" "} {login_box}
         <div className="main">
-          <QuestionList questionList={this.state.questionList} />
-=======
-          handle_logout={this.handle_logout}
-          logged_in={this.state.logged_in}
-          username={this.state.username}
-        />
-        {login_box}
-        <div className="main">
-          <QuestionList />
->>>>>>> 46e01a29e20e643ff7012936b85da5ecc4fccad7
+          <QuestionList questionList={this.state.questionList}/>
         </div>
         <div className="footer-area">
-          <Footer />
+          <Footer/>
         </div>
       </div>
     );
