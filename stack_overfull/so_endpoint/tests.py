9823ab5965e0823e75d3c1ecd9eb615d9c2e34ec
dobrovv <vlandobrov@gmail.com>
2018-02-17 23:24:42 -0500

    Added several test cases for backend task #99 - added test cases for user views

--- stack_overfull/so_endpoint/tests.py
+++ stack_overfull/so_endpoint/tests.py
@@ -1,3 +1,160 @@
 from django.test import TestCase
+from django.test import Client
+
+from so_endpoint.models import *
+import json
 
 # Create your tests here.
+
+def login_or_create(client, username, password):
+    """ Creates a new user if the user doesn't exist.
+        Logs the user in with passed username and password
+        Stores the session id in the passed client """
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_user(username=username, password=password)
+
+    is_logged_in = client.login(username=username, password=password)
+    return is_logged_in
+
+class UserViewTests(TestCase):
+
+    def test_user_view(self):
+        response = self.client.get('/api/user/')
+        user_list = response.json() # change array to user_list dict
+
+        self.assertIs(response.status_code, 200)
+        self.assertEqual(len(user_list), User.objects.count())
+
+class UserMeViewTests(TestCase):
+
+    def test_user_not_logged(self):
+        response = self.client.get('/api/user/me/')
+
+        self.assertIs(response.status_code, 200) # why 200
+        self.assertTrue('error' in response.json())
+
+    def test_user_logged(self):
+
+        is_logged_in = login_or_create(self.client, 'TestUser', '12345')
+        self.assertTrue(is_logged_in)
+
+        response = self.client.get('/api/user/me/')
+        response_body = response.json()
+
+        self.assertIs(response.status_code, 200)
+        self.assertIsNotNone(response_body['profile'])
+
+
+class UserRegisterViewTests(TestCase):
+
+    def test_valid_request(self):
+
+        json_payload = json.dumps({
+            "username": "TestUserRegister",
+            "password": "12345",
+            "email": "test@mail.com"
+        })
+
+        response = self.client.post(
+            '/api/user/register/',
+            data=json_payload,
+            content_type='application/json'
+        )
+
+        response_body = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_body['username'], 'TestUserRegister')
+        self.assertIsNotNone(response_body['profile'])
+
+    def test_invalid_request(self):
+
+        json_payload = json.dumps({
+            "username": None,
+            "password": "123"
+        })
+
+        response = self.client.post(
+            '/api/user/register/',
+            data=json_payload,
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_username_already_exists(self):
+        login_or_create(Client(), 'TestUsernameExists', '12345')
+
+        json_payload = json.dumps({
+            "username": "TestUsernameExists",
+            "password": "12345",
+            "email": "test@mail.com"
+        })
+
+        response = self.client.post(
+            '/api/user/register/',
+            data=json_payload,
+            content_type='application/json'
+        )
+
+        response_body = response.json()
+
+        self.assertEqual(response.status_code, 200) # why 200
+        self.assertTrue('error' in response_body)
+
+class UserLoginViewTests(TestCase):
+
+    def test_valid_request(self):
+
+        login_or_create(Client(), 'TestUserLogin', "12345")
+
+        json_payload = json.dumps({
+            "username": "TestUserLogin",
+            "password": "12345"
+        })
+
+        response = self.client.post(
+            '/api/user/login/',
+            data=json_payload,
+            content_type='application/json'
+        )
+
+        response_body = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_body['username'], 'TestUserLogin')
+        self.assertIsNotNone(response_body['profile'])
+
+    def test_invalid_request(self):
+
+        json_payload = json.dumps({
+            "username": None,
+            "password": "12345"
+        })
+
+        response = self.client.post(
+            '/api/user/register/',
+            data=json_payload,
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue('error' in response.json())
+
+    def test_invalid_password(self):
+        login_or_create(Client(), 'TestUserLogin', '12345')
+
+        json_payload = json.dumps({
+            "username": "TestUserLogin",
+            "password": "123xx"
+        })
+
+        response = self.client.post(
+            '/api/user/register/',
+            data=json_payload,
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 200) # why 200
+        self.assertTrue('error' in response.json())
