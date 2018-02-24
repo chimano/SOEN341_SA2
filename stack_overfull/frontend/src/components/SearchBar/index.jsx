301264b6a157e120e0e1bd06b0f7730c3ae64d38
dobrovv <vlandobrov@gmail.com>
2018-02-24 15:04:20 -0500

    Fix for redirect - added a reset for redirect - changed some names for readability

--- stack_overfull/frontend/src/components/SearchBar/index.jsx
+++ stack_overfull/frontend/src/components/SearchBar/index.jsx
@@ -1,63 +1,67 @@
 import React from 'react';
 import './index.css';
-import {withRouter} from "react-router-dom";
+
 import { Redirect } from 'react-router-dom';
-import { Link } from "react-router-dom";
+import qs from 'qs'
 
 export class SearchBar extends React.Component {
-    constructor (props) {
-      super(props);
-      this.state = {
-        search : '',
-        url : '',
-        redirect : false
-      }
-      // binding the context to the SearchBar 
-      // because this functions will be executed in a 
-      // different context
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSearch = this.handleSearch.bind(this);
-    }
-    // Modify the search key of the component state
-    handleChange (e) {
-      this.setState({
-        search : e.target.value
-      });
-    }
-    // gets the user input
-    // and build the url based on it
-    handleSearch (e) {
-      e.preventDefault(); // prevent page reload
-      const { search } = this.state;
-      const url = "/search/?q=" + search;
+  constructor (props) {
+    super(props);
 
-    console.log(url)
-    this.setState({
-        url,
-        redirect : true});
+    this.state = {
+      search_text : '',
+      search_url : '',
+      redirect : false
     }
-  
-    render () {
-      const { search, url, redirect } = this.state;
+  }
+
+
+  // Modify the search key of the component state
+  handleChange = (e) => {
+    this.setState({
+      search_text : e.target.value
+    });
+  }
+
+  // gets the user input
+  // and build the url based on it
+  handleSearch = (e) => {
+    e.preventDefault(); // prevent page reload
+    const { search_text } = this.state;
+    const search_url = "/search/?" + qs.stringify({q:search_text});
+
+    console.log(search_url)
+    this.setState({
+      search_url:  search_url,
+      redirect : true
+    });
+
+  }
+
+  render () {
+    const { search_text, search_url, redirect } = this.state;
 
     if (redirect) {
-          return <Redirect to={"/search/?q=" + search }/>
-    }
-  
-      
-      return (
-        <form onSubmit={this.handleSearch}>
-            <input 
-                type="text"
-                value={search}
-                placeholder="Search questions here"
-                onChange={this.handleChange}/>
+      console.log('Redirecting to', search_url)
+      // reset redirect without triggering a state change
+      this.state.redirect = false
 
-            <button type="submit" >
-                <Link to={"/search/?q=" + search} style={{ color: "white" }}>Search</Link>
-            </button>
-        </form>
-      );
+      return <Redirect to={search_url}/>
     }
-  
+
+    return (
+      <form onSubmit={this.handleSearch}>
+        <input
+          type="text"
+          value={search_text}
+          placeholder="Search questions here"
+          onChange={this.handleChange}/>
+
+        <button type="submit" style={{ color: "white" }}>
+          Search
+        </button>
+      </form>
+    );
   }
+
+}
