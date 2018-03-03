09046b76be2b5c3ed1a7e0a248f974108f0ad297
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2018-03-03 01:49:25 -0500

    Issue #126: - replaced function syntax with arrow function

--- stack_overfull/frontend/src/utils/api.js
+++ stack_overfull/frontend/src/utils/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 //get single question by id
-export function getApiQuestionById(id) {
+export const getApiQuestionById = id => {
   return axios.get("/api/question/", {
     params: {
       id: id
     }
   });
-}
+};
 
 //get the list of questions
-export function getApiQuestion(order, limit, sort) {
+export const getApiQuestion = (order, limit, sort) => {
   return axios.get("/api/question/", {
     params: {
       order: order,
@@ -18,10 +18,10 @@ export function getApiQuestion(order, limit, sort) {
       sort: sort
     }
   });
-}
+};
 
 //get answer by id of the question
-export function getApiAnswerById(id, order, limit) {
+export const getApiAnswerById = (id, order, limit) => {
   return axios.get("/api/answer/", {
     params: {
       q_id: id,
@@ -29,84 +29,84 @@ export function getApiAnswerById(id, order, limit) {
       limit: limit
     }
   });
-}
+};
 
 //get user info and check whether user is logged in or not
-export function getApiUserMe() {
+export const getApiUserMe = () => {
   return axios.get("/api/user/me");
-}
+};
 
 //post question
-export function postApiQuestion(question) {
+export const postApiQuestion = question => {
   return axios.post("/api/question/", {
     question_head: question.question_head,
     question_text: question.question_text
   });
-}
+};
 
 //post answer
-export function postApiAnswer(answer, q_id) {
+export const postApiAnswer = (answer, q_id) => {
   return axios.post("/api/answer/", { answer: answer, q_id: q_id });
-}
+};
 
 //vote on answer, return updated value of points
-export function voteAnswer(vote_type, a_id) {
+export const voteAnswer = (vote_type, a_id) => {
   return axios.post("/api/answer/vote/", {
     vote_type: vote_type,
     a_id: a_id
   });
-}
+};
 
 //login the user
-export function postApiUserLogin(username, password) {
+export const postApiUserLogin = (username, password) => {
   return axios.post("/api/user/login/", {
     username: username,
     password: password
   });
-}
+};
 
 //logout the user
-export function postApiUserLogout() {
+export const postApiUserLogout = () => {
   return axios.post("/api/user/logout/");
-}
+};
 
 //register user
-export function postApiUserRegister(username, password, email) {
+export const postApiUserRegister = (username, password, email) => {
   return axios.post("/api/user/register/", {
     username: username,
     password: password,
     email: email
   });
-}
+};
 
 //Accept answer
-export function postApiAnswerIdAccept(id) {
+export const postApiAnswerIdAccept = id => {
   return axios.post("/api/answer/" + id + "/accept/");
-}
+};
 
 //Reject answer
-export function postApiAnswerIdReject(id) {
+export const postApiAnswerIdReject = id => {
   return axios.post("/api/answer/" + id + "/reject/");
-}
+};
 
 //Undo accept answer
-export function postApiAnswerIdAcceptUndo(id) {
+export const postApiAnswerIdAcceptUndo = id => {
   return axios.post("/api/answer/" + id + "/accept/undo/");
-}
+};
 
 //Undo reject answer
-export function postApiAnswerIdRejectUndo(id) {
+export const postApiAnswerIdRejectUndo = id => {
   return axios.post("/api/answer/" + id + "/reject/undo/");
-}
+};
 
 //get the list of questions matching the query ('q')
 //returns a question_list in response.data
-export function getApiSearch(
+export const getApiSearch = (
   q = null,
   order = "desc",
   limit = 10,
   sort = "date_created"
-) {
+) => {
   return axios.get("/api/search/", {
     params: {
       q: q, // the query string
@@ -115,4 +115,4 @@ export function getApiSearch(
       sort: sort
     }
   });
-}
+};
