3f2b797af2a5372df53f5071c75e76d0a76ea837
chimano <chiracmanoukian@hotmail.com>
2018-03-05 14:21:06 -0500

    Improved code quality #133
    - Improved quality of code by following certain guidelines
    - Added some comments to clarify the code

--- stack_overfull/so_endpoint/views.py
+++ stack_overfull/so_endpoint/views.py
@@ -12,13 +12,21 @@ import json
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+
 class QuestionView(TemplateView):
+    """
+    This view is designed to handle the /api/question/ endpoint.
+    It's used to create questions through a POST request or to
+    get a question with a GET request
+    """
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(QuestionView, self).dispatch(request, *args, **kwargs)
 
     def post(self, request):
         try:
+            # Extracts question info from request
             json_data = json.loads(request.body)
             question_head = json_data['question_head']
             question_text = json_data['question_text']
@@ -27,9 +35,12 @@ class QuestionView(TemplateView):
             except ObjectDoesNotExist:
                 return JsonResponse({'error': 'User is not logged in'}, status=400)
 
+            # Checks if question has a valid length
             if len(question_head) <= 1:
                 return JsonResponse({'error': 'Question length is invalid'}, status=400)
-            q = Question(question_head=question_head, question_text=question_text, user_id=user)
+            q = Question(question_head=question_head,
+                         question_text=question_text, user_id=user)
+
             q.save()
             return JsonResponse({'id': q.id})
 
@@ -37,19 +48,23 @@ class QuestionView(TemplateView):
             return JsonResponse({'error': 'There was an error parsing the request'}, status=400)
 
     def get(self, request):
+        #Extracts GET request parameters
         limit = request.GET.get('limit', 10)
         q_id = request.GET.get('id', None)
         order = request.GET.get('order', 'desc')
         sorted_by = request.GET.get('sort', 'points')
         if q_id is None:
+            #If q_id is not set then it returns a list of questions
             limit = 10 if limit is None else int(limit)
             order = "desc" if order is None else order
             modifier = '-' if order == 'desc' else ''
 
-            questions = Question.objects.all().order_by(modifier + sorted_by)[:limit]
+            questions = Question.objects.all().order_by(
+                modifier + sorted_by)[:limit]
             serialized = QuestionSerializer(questions, many=True).data
-            return JsonResponse({'question_list':serialized})
+            return JsonResponse({'question_list': serialized})
         else:
+            #Returns a single question
             try:
                 question = Question.objects.get(id=q_id)
             except:
@@ -59,6 +74,11 @@ class QuestionView(TemplateView):
 
 
 class AnswerView(TemplateView):
+    """
+    This view is designed to handle the /api/answer/ endpoint.
+    It's used to create answers through a POST request or to
+    get a list of answers with a GET request
+    """
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(AnswerView, self).dispatch(request, *args, **kwargs)
@@ -66,20 +86,24 @@ class AnswerView(TemplateView):
     def post(self, request, id=None):
 
         try:
+            #Extracts data from request
             json_data = json.loads(request.body)
             answer = json_data['answer']
             q_id = int(json_data['q_id'])
 
+            #Checks if user is logged in
             try:
                 user = User.objects.get(username=request.user)
             except ObjectDoesNotExist:
                 return JsonResponse({'error': 'User is not logged in'}, status=400)
 
+            #Checks to see if the question that's getting answered exists
             try:
                 q = Question.objects.get(id=q_id)
             except ObjectDoesNotExist:
                 return JsonResponse({'error': 'Question does not exist'}, status=400)
 
+            #Checks to see if answer has valid length
             if len(answer) <= 1:
                 return JsonResponse({'error': 'Answer length is invalid'}, status=400)
             a = Answer(question_id=q, answer_text=answer, user_id=user)
@@ -93,23 +117,29 @@ class AnswerView(TemplateView):
     def get(self, request):
 
         try:
+            # Gets a list of answers from the question id
             limit = int(request.GET.get('limit', 10))
             order = request.GET.get('order', 'desc')
             q_id = request.GET.get('q_id', None)
             sorted_by = request.GET.get('sort', 'points')
             modifier = '-' if order == 'desc' else ''
+
+            #Checks to see if question exists
             try:
                 q = Question.objects.get(id=q_id)
             except ObjectDoesNotExist:
                 return JsonResponse({'error': 'Question does not exist'}, status=400)
 
-            answers = Answer.objects.filter(question_id=q_id).order_by(modifier + sorted_by)[:limit]
+            answers = Answer.objects.filter(question_id=q_id).order_by(
+                modifier + sorted_by)[:limit]
             serialized = AnswerSerializer(answers, many=True).data
-            return JsonResponse({'answer_list':serialized})
+            return JsonResponse({'answer_list': serialized})
         except KeyError:
             return JsonResponse({'error': 'There was an error parsing the request'}, status=400)
 
-#Accept or undo accept an answer
+# Accept or undo accept an answer
+
+
 class AnswerAcceptView(TemplateView):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
@@ -121,7 +151,7 @@ class AnswerAcceptView(TemplateView):
             answer = Answer.objects.get(pk=answer_id)
             to_question = answer.question_id
 
-        except ObjectDoesNotExist as e: # nonexistent answer_id
+        except ObjectDoesNotExist as e:  # nonexistent answer_id
             print(repr(e))
             return JsonResponse({'error': repr(e)}, status=400)
 
@@ -131,7 +161,7 @@ class AnswerAcceptView(TemplateView):
         if to_question.user_id != request.user:
             return JsonResponse({'error': 'User is not the author of the question'}, status=400)
 
-        #if question.accepted_answer_id is not None and undo is False:
+        # if question.accepted_answer_id is not None and undo is False:
         #    return JSONResponse({'error': 'An answer to the question is alreay accepted'})
 
         if undo is True and to_question.accepted_answer_id != answer:
@@ -144,6 +174,7 @@ class AnswerAcceptView(TemplateView):
         to_question.save()
         return JsonResponse(QuestionSerializer(to_question).data)
 
+
 class AnswerRejectView(TemplateView):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
@@ -155,7 +186,7 @@ class AnswerRejectView(TemplateView):
             answer = Answer.objects.get(pk=answer_id)
             to_question = answer.question_id
 
-        except ObjectDoesNotExist as e: # nonexistent answer_id
+        except ObjectDoesNotExist as e:  # nonexistent answer_id
             print(repr(e))
             return JsonResponse({'error': repr(e)}, status=400)
 
@@ -175,6 +206,7 @@ class AnswerRejectView(TemplateView):
         to_question.save()
         return JsonResponse(QuestionSerializer(to_question).data)
 
+
 class UserNameView(TemplateView):
 
     @method_decorator(csrf_exempt)
@@ -191,6 +223,8 @@ class UserNameView(TemplateView):
             return JsonResponse({'error': 'User does not exist'}, status=400)
 
 # Return the list of users in the database
+
+
 class UserView(TemplateView):
 
     @method_decorator(csrf_exempt)
@@ -208,14 +242,14 @@ class UserView(TemplateView):
             limit = int(limit)
             modifier = '-' if order == "desc" else ''
 
-            users =  User.objects.select_related('profile')
+            users = User.objects.select_related('profile')
             users = users.filter(username__contains=inname)
             users = users.order_by(modifier+sorted_by)[:limit]
             users_list = AccountSerializerPublic(users, many=True).data
 
             return JsonResponse({'user_list': users_list})
 
-        except BaseException as e: # either TypeError or ValueError from query params
+        except BaseException as e:  # either TypeError or ValueError from query params
             print(repr(e))
             return JsonResponse({'error': repr(e)}, status=400)
 
@@ -261,7 +295,7 @@ class UserMeView(TemplateView):
 
             return JsonResponse(AccountSerializerPrivate(user).data)
 
-        except BaseException as e: # json or user, user.profile or request body params don't match db
+        except BaseException as e:  # json or user, user.profile or request body params don't match db
             print(repr(e))
             return JsonResponse({'error': repr(e)}, status=400)
 
@@ -285,17 +319,20 @@ class UserRegisterView(TemplateView):
             if User.objects.filter(username=username).exists():
                 return JsonResponse({'error': 'Username already exists'}, status=400)
 
-            User.objects.create_user(username=username, password=password, email=email)
+            User.objects.create_user(
+                username=username, password=password, email=email)
             user = authenticate(request, username=username, password=password)
             login(request, user)
 
             return JsonResponse(AccountSerializerPrivate(user).data)
 
-        except BaseException as e: #either a json, key or user validation error
+        except BaseException as e:  # either a json, key or user validation error
             print(repr(e))
             return JsonResponse({'error': repr(e)}, status=400)
 
-#Log the user in
+# Log the user in
+
+
 class UserLoginView(TemplateView):
 
     @method_decorator(csrf_exempt)
@@ -318,8 +355,7 @@ class UserLoginView(TemplateView):
             else:
                 return JsonResponse({'error': 'Wrong username/password'}, status=400)
 
-
-        except BaseException as e: #either a json or key error
+        except BaseException as e:  # either a json or key error
             print(str(e))
             return JsonResponse({'error': repr(e)}, status=400)
 
@@ -334,28 +370,34 @@ class UserLogoutView(TemplateView):
     def post(self, request):
         if request.user.is_authenticated:
             logout(request)
-            return JsonResponse({'status':'done'})
+            return JsonResponse({'status': 'done'})
         else:
             return JsonResponse({'error': 'User is not logged in'}, status=400)
 
 
 class AnswerVoteView(TemplateView):
-
+    """
+    This view is used to handle the /api/answer/vote/ endpoint.
+    It is used to vote on a given answer through a post request
+    """
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request):
         try:
+            #Extracts information from request
             json_data = json.loads(request.body)
             a_id = json_data['a_id']
+
+            #Checks to see if answer exists
             try:
                 answer = Answer.objects.get(id=a_id)
             except Answer.DoesNotExist:
                 return JsonResponse({'error': 'Answer id is not valid'}, status=400)
 
             vote_type = json_data['vote_type']
-
+            #Checks to see if user is logged in
             if request.user.is_authenticated:
                 user = request.user
             else:
@@ -365,9 +407,11 @@ class AnswerVoteView(TemplateView):
             return JsonResponse({'error': 'There was an error extracting the parameters'}, status=400)
 
         if vote_type == "UP":
+            #Checks to see if the user already upvoted for answer
             if answer in user.profile.upvoted_answers.all():
                 return JsonResponse({'error': 'User has already voted for this answer'}, status=400)
-
+            
+            #Checks to see if user has previously downvoted the answer
             if answer in user.profile.downvoted_answers.all():
                 user.profile.downvoted_answers.remove(answer)
                 answer.points += 1
@@ -378,19 +422,23 @@ class AnswerVoteView(TemplateView):
 
             user.profile.upvoted_answers.add(answer)
             answer.points += 1
+
+            #Increments user's reputation,
             try:
                 answer.user_id.profile.update_profile_reputation(1)
             except:
+                #It should never come to this section
                 print("Answer has no user")
             answer.save()
             return JsonResponse({'sucess': 'Upvoted the answer',
                                  'points': answer.points}, status=200)
 
         elif vote_type == "DOWN":
-
+            #Checks to see if user has previously downvoted the answer
             if answer in user.profile.downvoted_answers.all():
                 return JsonResponse({'error': 'User has already voted for this answer'}, status=400)
 
+            #Checks to see if user has previously upvoted the answer
             if answer in user.profile.upvoted_answers.all():
                 user.profile.upvoted_answers.remove(answer)
                 answer.points -= 1
@@ -411,15 +459,22 @@ class AnswerVoteView(TemplateView):
 
 
 class QuestionVoteView(TemplateView):
-
+    """
+    This view is used to handle the /api/question/vote/ endpoint.
+    It is used to vote on a given question through a post request
+    """
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request):
+        # This method handles the question voting
         try:
+            #Extracts parameters from requests
             json_data = json.loads(request.body)
             q_id = json_data['q_id']
+
+            #Checks to see if question exists
             try:
                 question = Question.objects.get(id=q_id)
             except Question.DoesNotExist:
@@ -427,6 +482,7 @@ class QuestionVoteView(TemplateView):
 
             vote_type = json_data['vote_type']
 
+            #Checks to see if user is logged in
             if request.user.is_authenticated:
                 user = request.user
             else:
@@ -435,6 +491,7 @@ class QuestionVoteView(TemplateView):
         except:
             return JsonResponse({'error': 'There was an error extracting the parameters'}, status=400)
 
+        #Handles upvotes and downvotes same way as the AnswerVoteView does
         if vote_type == "UP":
             if question in user.profile.upvoted_questions.all():
                 return JsonResponse({'error': 'User has already voted for this question'}, status=400)
@@ -455,12 +512,12 @@ class QuestionVoteView(TemplateView):
                 print("Question has no user")
             question.save()
             return JsonResponse({'sucess': 'Upvoted the question',
-                                 'points': question.points},status=200)
+                                 'points': question.points}, status=200)
 
         elif vote_type == "DOWN":
 
             if question in user.profile.downvoted_questions.all():
-                return JsonResponse({'error': 'User has already voted for this question'},status=400)
+                return JsonResponse({'error': 'User has already voted for this question'}, status=400)
 
             if question in user.profile.upvoted_questions.all():
                 user.profile.upvoted_questions.remove(question)
@@ -478,7 +535,8 @@ class QuestionVoteView(TemplateView):
                 print("Question has no user")
             question.save()
             return JsonResponse({'sucess': 'Downvoted the question',
-                                 'points': question.points},status=200)
+                                 'points': question.points}, status=200)
+
 
 class SearchView(TemplateView):
     @method_decorator(csrf_exempt)
@@ -486,7 +544,7 @@ class SearchView(TemplateView):
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
-        try :
+        try:
             query = request.GET.get('q', '')
             limit = request.GET.get('limit', 10)
             order = request.GET.get('order', 'desc')
@@ -495,19 +553,22 @@ class SearchView(TemplateView):
             limit = int(limit)
             modifier = '-' if order == "desc" else ''
 
-            #filter questions by case-insensitive matching with question_text
-            by_question_text = Question.objects.filter(question_text__icontains=query)
+            # filter questions by case-insensitive matching with question_text
+            by_question_text = Question.objects.filter(
+                question_text__icontains=query)
 
-            #filter questions by case-insensitive matching with username
-            by_username = Question.objects.filter(user_id__username__icontains=query)
+            # filter questions by case-insensitive matching with username
+            by_username = Question.objects.filter(
+                user_id__username__icontains=query)
 
             matching_questions = by_question_text | by_username
 
-            matching_questions = matching_questions.order_by(modifier+sorted_by)[:limit]
+            matching_questions = matching_questions.order_by(
+                modifier+sorted_by)[:limit]
 
             serialized = QuestionSerializer(matching_questions, many=True).data
             return JsonResponse({'question_list': serialized})
 
-        except BaseException as e: # either TypeError or ValueError from query params
+        except BaseException as e:  # either TypeError or ValueError from query params
             print(repr(e))
             return JsonResponse({'error': repr(e)}, status=400)
