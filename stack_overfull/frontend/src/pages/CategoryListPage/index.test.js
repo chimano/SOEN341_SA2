ea0dbee141db45bf0aea853ed37421f6093c5f65
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2018-03-06 13:34:53 -0500

    - fixed categoryListPage test #89

--- stack_overfull/frontend/src/pages/CategoryListPage/index.test.js
+++ stack_overfull/frontend/src/pages/CategoryListPage/index.test.js
@@ -1,20 +1,18 @@
 import React from "react";
-import { CategoryPage } from "./index.jsx";
+import { CategoryListPage } from "./index.jsx";
 import { configure, shallow, mount, render } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
 
-describe("<CategoryPage />", () => {
+describe("<CategoryListPage />", () => {
   let component;
 
   beforeEach(() => {
-    component = mount(
-      <CategoryPage />
-    );
+    component = shallow(<CategoryListPage />);
   });
 
-  it("renders 1 <CategoryPage /> component", () => {
+  it("renders 1 <CategoryListPage /> component", () => {
     expect(component).toHaveLength(1);
   });
 });
