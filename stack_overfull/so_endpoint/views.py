45d56e696e6fb171fa09a0643f9cf41d8ff59c6d
chimano <chiracmanoukian@hotmail.com>
2018-03-09 13:48:00 -0500

    Fixed Travis-CI codecov bug #169

--- stack_overfull/so_endpoint/views.py
+++ stack_overfull/so_endpoint/views.py
@@ -584,7 +584,7 @@ class SearchView(TemplateView):
             by_username = Question.objects.filter(
                 user_id__username__icontains=query)
 
-            matching_questions =by_question_head | by_question_text | by_username
+            matching_questions = by_question_head | by_question_text | by_username
 
             matching_questions = matching_questions.order_by(
                 modifier+sorted_by)[:limit]
