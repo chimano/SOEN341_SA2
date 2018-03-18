92b2ddeb828599138910659aaf0d81b21b1aa885
vincent.sun870530@gmail.com <vincent.sun870530@gmail.com>
2018-03-18 19:55:42 -0400

    issue#160 add new user page

---
+++ stack_overfull/frontend/src/pages/UserPage/index.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { ProfilePage } from "./index.jsx";
+import { configure, shallow, mount, render } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+describe("<UserPage />", () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(
+      <UserPage />
+    );
+  });
+
+  it("renders 1 <UserPage /> component", () => {
+    expect(component).toHaveLength(1);
+  });
+});
