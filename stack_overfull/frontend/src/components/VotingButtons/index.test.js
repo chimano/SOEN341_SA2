7a198191779a9af5118b2a289f89b596c3cb98ed
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2018-03-20 20:24:25 -0400

    added test

--- stack_overfull/frontend/src/components/VotingButtons/index.test.js
+++ stack_overfull/frontend/src/components/VotingButtons/index.test.js
@@ -7,8 +7,17 @@ configure({ adapter: new Adapter() });
 
 describe("<VotingButtons />", () => {
   const points = 10;
+  const upvoted_array = [1, 4, 6];
+  const downvoted_array = [3, 7, 9];
+
   it("renders 1 <VotingButtons /> component", () => {
-    const component = shallow(<VotingButtons points={points}/>);
+    const component = shallow(
+      <VotingButtons
+        points={points}
+        upvoted_array={upvoted_array}
+        downvoted_array={downvoted_array}
+      />
+    );
     expect(component).toHaveLength(1);
   });
 });
