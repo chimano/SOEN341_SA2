37d58c8cc56f3f0640545ba8d0bb35f64a4897dd
Kevin <lo_kevin@outlook.com>
2018-03-25 23:33:24 -0400

    Updated the ui on profile page #210
    -Added a new component for tabs in the profile page
    -Added tabs for the different type of questions
    -Modified the ui of profile information and the types of questions

--- stack_overfull/frontend/src/components/UserQuestionList/index.jsx
+++ stack_overfull/frontend/src/components/UserQuestionList/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css";
 import { QuestionList } from "../../components";
 
 export class UserQuestionList extends React.Component {
@@ -7,18 +8,45 @@ export class UserQuestionList extends React.Component {
       upvoted_questions,
       downvoted_questions,
       questions_asked,
-      questions_answered
+      questions_answered,
+      current_tab
     } = this.props;
+
+    let title;
+    let showQuestions;
+    if(current_tab == "questionasked") {
+      title = ( <h3>Questions Asked</h3> );
+      showQuestions = (
+        <QuestionList questionList={questions_asked} />
+      );
+    } else if(current_tab == "questionanswered") {
+      title = ( <h3>Questions Answered</h3> );
+      showQuestions = (
+        <QuestionList questionList={questions_answered} />
+      );
+    } else if(current_tab == "upvotedquestion") {
+      title = ( <h3>Upvoted Questions</h3> );
+      showQuestions = (
+        <QuestionList questionList={upvoted_questions} />
+      );
+    } else if(current_tab == "questionasked") {
+      title = ( <h3>Question Asked</h3> );
+      showQuestions = (
+        <QuestionList questionList={downvoted_questions} />
+      );
+    }
     return (
-      <div style={{ paddingTop: "30px" }}>
-        <h3>Upvoted Questions</h3>
+      <div style={{ paddingTop: "30px"}}>
+        {title}
+        {showQuestions}
+        {/* <h3>Upvoted Questions</h3>
         <QuestionList questionList={upvoted_questions} />
         <h3>Downvoted Questions</h3>
         <QuestionList questionList={downvoted_questions} />
         <h3>Questions Asked</h3>
         <QuestionList questionList={questions_asked} />
         <h3>Questions Answered</h3>
-        <QuestionList questionList={questions_answered} />
+        <QuestionList questionList={questions_answered} /> */}
       </div>
     );
   }
